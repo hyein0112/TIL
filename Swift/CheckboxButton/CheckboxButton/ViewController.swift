75a15b5666e0f7ee12a947edd68d9816f7556687
hyein0112 <bhi12134@gmail.com>
2021-06-24 23:29:57 +0900

    210624 [UPDATE] CheckboxButton

--- Swift/CheckboxButton/CheckboxButton/ViewController.swift
+++ Swift/CheckboxButton/CheckboxButton/ViewController.swift
@@ -11,8 +11,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var checkbox1: UIButton!
     @IBOutlet weak var checkbox2: UIButton!
     @IBOutlet weak var checkbox3: UIButton!
+    @IBOutlet weak var allCheckbox: UIButton!
     
-    var flag1 = false, flag2 = false, flag3=false
+    var flag1 = false, flag2 = false, flag3=false, allFlag = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +25,15 @@ class ViewController: UIViewController {
         if flag1 == false{
             checkbox1.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
             flag1 = true
+            if flag2 == true && flag3 == true{
+                allCheckbox.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
+                allFlag = true
+            }
         }else if flag1 == true{
             checkbox1.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            allCheckbox.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
             flag1 = false
+            allFlag = false
         }
     }
     
@@ -35,9 +42,15 @@ class ViewController: UIViewController {
         if flag2 == false{
             checkbox2.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
             flag2 = true
+            if flag1 == true && flag3 == true{
+                allCheckbox.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
+                allFlag = true
+            }
         }else if flag2 == true{
             checkbox2.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            allCheckbox.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
             flag2 = false
+            allFlag = false
         }
     }
     @IBAction func isChecked3(_ sender: UIButton) {
@@ -45,10 +58,38 @@ class ViewController: UIViewController {
         if flag3 == false{
             checkbox3.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
             flag3 = true
+            if flag1 == true && flag2 == true{
+                allCheckbox.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
+                allFlag = true
+            }
         }else if flag3 == true{
             checkbox3.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            allCheckbox.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            allFlag = false
             flag3 = false
         }
     }
+    @IBAction func isAllChecked(_ sender: UIButton) {
+        if allFlag == false{
+            allCheckbox.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
+            checkbox1.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
+            checkbox2.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
+            checkbox3.setBackgroundImage(UIImage(named: "EZY_CheckedBox"), for: .normal)
+            allFlag = true
+            flag1 = true
+            flag2 = true
+            flag3 = true
+        }else if allFlag == true{
+            allCheckbox.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            checkbox1.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            checkbox2.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            checkbox3.setBackgroundImage(UIImage(named: "EZY_UncheckedBox"), for: .normal)
+            allFlag = false
+            flag1 = false
+            flag2 = false
+            flag3 = false
+        }
+    }
+    
 }
 
