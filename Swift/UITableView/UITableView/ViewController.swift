e5ad6864a9fad227e7e1ead6335c38984dffd71e
hyein0112 <bhi12134@gmail.com>
2021-06-28 20:59:24 +0900

    210628 [UPDATE] UITableView

--- Swift/UITableView/UITableView/ViewController.swift
+++ Swift/UITableView/UITableView/ViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.numbers.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text : String = self.numbers[indexPath.row]
+        cell.textLabel?.text = text
+           return cell
+    }
+   
+  
+    @IBOutlet weak var navigationBar: UINavigationBar!
+
+     
+
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier : String = "cell"
+    let numbers : [String] = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" ]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationBar.topItem?.title = "Numbers"
         // Do any additional setup after loading the view.
     }
 
