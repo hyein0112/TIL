30d2ae45a5a78c16e61d0463ca2de133f590ad3b
hyein0112 <bhi12134@gmail.com>
2021-08-29 18:44:46 +0900

    210829 [UPDATE]

--- Swift/AutoLayout_Snapkit/AutoLayout_Snapkit/ViewController.swift
+++ Swift/AutoLayout_Snapkit/AutoLayout_Snapkit/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
 
@@ -16,4 +18,31 @@ class ViewController: UIViewController {
 
 
 }
+
+//MARK: - Preview
+
+#if DEBUG
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        ViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
 
---
+++ Swift/AutoLayout_Snapkit/Pods/Target Support Files/Pods-AutoLayout_Snapkit/Pods-AutoLayout_Snapkit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AutoLayout_Snapkit : NSObject
+@end
+@implementation PodsDummy_Pods_AutoLayout_Snapkit
+@end
