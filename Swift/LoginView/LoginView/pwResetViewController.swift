830f94a367904942eb0856b58e1e2c4d3370ee0e
hyein0112 <bhi12134@gmail.com>
2021-09-01 19:57:52 +0900

    210901 [UPDATE] pwResetView

--- Swift/LoginView/LoginView/pwResetViewController.swift
+++ Swift/LoginView/LoginView/pwResetViewController.swift
@@ -15,7 +15,7 @@ class pwResetViewController: UIViewController {
     var mainLabel = UILabel().then {
         $0.text = "ASSIGNMENT"
         $0.textColor = .white
-        $0.font = .systemFont(ofSize: 30)
+        $0.font = UIFont(name: "Apple SD Gothic Neo", size: 30)
     }
     var backgroundView = UIView().then {
         $0.backgroundColor = .white
@@ -23,35 +23,38 @@ class pwResetViewController: UIViewController {
     }
     var titleLabel = UILabel().then {
         $0.text = "비밀번호 재설정"
-        $0.font = .systemFont(ofSize: 25)
-        $0.textColor = .darkGray
+        $0.font = UIFont(name: "Apple SD Gothic Neo", size: 25)
+        $0.textColor = UIColor(red: 118/255, green: 118/255, blue: 118/255, alpha: 1)
     }
     var idLabel = UILabel().then {
         $0.text = "아이디"
-        $0.textColor = .darkGray
+        $0.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        $0.textColor = UIColor(red: 150/255, green: 150/255, blue: 150/255, alpha: 1)
     }
     var callNumLabel = UILabel().then {
         $0.text = "전화번호"
-        $0.textColor = .darkGray
+        $0.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        $0.textColor = UIColor(red: 150/255, green: 150/255, blue: 150/255, alpha: 1)
     }
     var certificationNumLabel = UILabel().then {
         $0.text = "인증번호"
+        $0.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
         $0.textColor = UIColor(red: 76/255, green: 96/255, blue: 144/255, alpha: 1)
     }
     var idField = UITextField().then {
         $0.placeholder = "id를 입력하세요"
-        $0.backgroundColor = UIColor(red: 236/255, green: 236/255, blue: 236/255, alpha: 1)
+        $0.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
         $0.layer.cornerRadius = 5
     }
     var callNumField = UITextField().then {
         $0.placeholder = "전화번호를 입력하세요"
-        $0.backgroundColor = UIColor(red: 236/255, green: 236/255, blue: 236/255, alpha: 1)
+        $0.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
         $0.layer.cornerRadius = 5
     }
     var certificationPhoneNumButton = UIButton().then {
         $0.setTitle("번호 인증", for: .normal)
         $0.backgroundColor = UIColor(red: 76 / 255, green: 96 / 255, blue: 144 / 255, alpha : 1)
-        $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 12)
+        $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 10)
         $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 10
     }
@@ -62,7 +65,7 @@ class pwResetViewController: UIViewController {
     }
     var pwResetButton = UIButton().then {
         $0.setTitle("비밀번호 바꾸러 가기", for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.titleLabel?.font = .systemFont(ofSize: 13)
         $0.backgroundColor = UIColor(red: 76 / 255, green: 96 / 255, blue: 144 / 255, alpha : 1)
         $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 10
@@ -71,7 +74,7 @@ class pwResetViewController: UIViewController {
     var loginButton = UIButton().then {
         $0.setTitle("로그인 하러 가기", for: .normal)
         $0.setTitleColor(UIColor(red: 51 / 255, green: 51 / 255, blue: 51 / 255, alpha : 1), for: .normal)
-        $0.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        $0.titleLabel?.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
         $0.addTarget(self, action: #selector(tablogin), for: .touchUpInside)
     }
     override func viewDidLoad() {
@@ -100,70 +103,74 @@ class pwResetViewController: UIViewController {
             make.center.equalToSuperview()
         }
         mainLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(60)
+            make.width.equalToSuperview().dividedBy(2.21)
+            make.height.equalToSuperview().dividedBy(22.56)
+            make.top.equalToSuperview().offset(self.view.frame.height/14.25)
             make.centerX.equalToSuperview()
         }
-        backgroundView.snp.makeConstraints { (make) in
-            make.width.equalTo(340)
-            make.height.equalTo(414)
-            make.top.equalTo(160)
+        backgroundView.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.1)
+            make.height.equalToSuperview().dividedBy(1.96)
+            make.top.equalToSuperview().offset(self.view.frame.height/5.21)
             make.centerX.equalToSuperview()
         }
         titleLabel.snp.makeConstraints { (make) in
-            make.width.equalTo(165)
-            make.height.equalTo(31)
+            make.width.equalToSuperview().dividedBy(2.36)
+            make.height.equalToSuperview().dividedBy(26.19)
             make.centerX.equalToSuperview()
-            make.centerY.equalTo(backgroundView.snp.top).offset(50)
+            make.top.equalToSuperview().offset(self.view.frame.height/3.96)
         }
         idLabel.snp.makeConstraints { (make) in
-            make.width.height.equalTo(self.idLabel)
-            make.left.equalTo(backgroundView.snp.left).offset(20)
-            make.top.equalTo(backgroundView.snp.top).offset(110)
+            make.width.equalToSuperview().dividedBy(11.03)
+            make.height.equalToSuperview().dividedBy(50.75)
+            make.left.equalToSuperview().offset(self.view.frame.width/10.14)
+            make.top.equalToSuperview().offset(self.view.frame.height/2.8)
         }
         callNumLabel.snp.makeConstraints { (make) in
-            make.width.height.equalTo(self.idLabel)
-            make.left.equalTo(backgroundView.snp.left).offset(20)
-            make.top.equalTo(idLabel.snp.bottom).offset(35)
+            make.width.equalToSuperview().dividedBy(7.21)
+            make.height.equalToSuperview().dividedBy(50.75)
+            make.left.equalTo(idLabel)
+            make.top.equalToSuperview().offset(self.view.frame.height/2.38)
         }
         certificationPhoneNumButton.snp.makeConstraints { (make) in
-            make.width.equalTo(59)
-            make.height.equalTo(22)
-            make.right.equalTo(callNumField).inset(3)
-            make.bottom.equalTo(callNumField).inset(7)
+            make.width.equalToSuperview().dividedBy(6.36)
+            make.height.equalToSuperview().dividedBy(36.91)
+            make.right.equalToSuperview().inset(self.view.frame.width/9.62)
+            make.top.equalToSuperview().offset(self.view.frame.height/2.41)
         }
         certificationNumLabel.snp.makeConstraints { (make) in
-            make.width.height.equalTo(self.idLabel)
-            make.left.equalTo(backgroundView.snp.left).offset(20)
-            make.top.equalTo(callNumLabel.snp.bottom).offset(35)
+            make.width.equalToSuperview().dividedBy(7.21)
+            make.height.equalToSuperview().dividedBy(50.75)
+            make.left.equalTo(idLabel)
+            make.top.equalToSuperview().offset(self.view.frame.height/2.07)
         }
         idField.snp.makeConstraints { (make) in
-            make.width.equalTo(240)
-            make.height.equalTo(36)
-            make.centerY.equalTo(idLabel.snp.centerY)
-            make.left.equalTo(idLabel.snp.right).offset(5)
+            make.width.equalToSuperview().dividedBy(1.5)
+            make.height.equalToSuperview().dividedBy(22.56)
+            make.top.equalToSuperview().offset(self.view.frame.height/2.91)
+            make.left.equalToSuperview().offset(self.view.frame.width/4.21)
         }
         callNumField.snp.makeConstraints { (make) in
-            make.width.equalTo(240)
-            make.height.equalTo(36)
-            make.left.equalTo(idField.snp.left)
-            make.centerY.equalTo(callNumLabel.snp.centerY)
+            make.width.height.equalTo(idField)
+            make.left.equalTo(idField)
+            make.top.equalToSuperview().offset(self.view.frame.height/2.46)
         }
         certificationNumField.snp.makeConstraints { (make) in
-            make.width.equalTo(240)
-            make.height.equalTo(36)
-            make.left.equalTo(idField.snp.left)
-            make.centerY.equalTo(certificationNumLabel.snp.centerY)
+            make.width.height.equalTo(idField)
+            make.left.equalTo(idField)
+            make.top.equalToSuperview().offset(self.view.frame.height/2.13)
         }
         pwResetButton.snp.makeConstraints { (make) in
-            make.width.equalTo(272)
-            make.height.equalTo(36)
+            make.width.equalToSuperview().dividedBy(1.38)
+            make.height.equalTo(idField)
             make.centerX.equalToSuperview()
-            make.top.equalTo(certificationNumField.snp.bottom).offset(40)
+            make.top.equalToSuperview().offset(self.view.frame.height/1.85)
         }
         loginButton.snp.makeConstraints { (make) in
-            make.width.height.equalTo(self.loginButton)
+            make.width.equalToSuperview().dividedBy(4.36)
+            make.height.equalToSuperview().dividedBy(50.75)
             make.centerX.equalToSuperview()
-            make.centerY.equalTo(backgroundView.snp.bottom).inset(40)
+            make.top.equalToSuperview().offset(self.view.frame.height/1.56)
         }
     }
     
