1b1e8158270239132ef3ed4be3bef8c807f239cb
hyein0112 <bhi12134@gmail.com>
2021-09-23 16:57:09 +0900

    210923 [CREATE] COVID Tracker

---
+++ Swift/Programmatically/COVID Tracker/COVID Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID Tracker
+//
+//  Created by GSM02 on 2021/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
