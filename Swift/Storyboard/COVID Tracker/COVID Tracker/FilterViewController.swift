763e792e450a4c99161cfa893f4bf7fc53a2dc4b
hyein0112 <bhi12134@gmail.com>
2021-09-25 02:33:11 +0900

    210925 [UPDATE]

--- Swift/Storyboard/COVID Tracker/COVID Tracker/FilterViewController.swift
+++ Swift/Storyboard/COVID Tracker/COVID Tracker/FilterViewController.swift
@@ -20,7 +20,26 @@ class FilterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Select State"
-        
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func vidwDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: IndexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableVIew.deselectRow(at: indexPath, animated: true)
     }
 
 }
