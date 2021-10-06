9b0af47a85be16c3e703cbd7bed0b0f25ab0bf1f
hyein0112 <bhi12134@gmail.com>
2021-10-06 19:54:44 +0900

    211006 [CREATE] Spotify

---
+++ Swift/Programmatically/Spotify/Spotify/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by GSM02 on 2021/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
