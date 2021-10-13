76c2f2efdb7791ea094735fc528c4383a6614c58
hyein0112 <bhi12134@gmail.com>
2021-10-13 20:35:03 +0900

    211013 [UPDATE] MyCollectionView

--- Swift/Programmatically/MyCollectionView/MyCollectionView/AppDelegate.swift
+++ Swift/Programmatically/MyCollectionView/MyCollectionView/AppDelegate.swift
@@ -1,4 +1,4 @@
-//
+///Users/gsm02/TIL/Swift/Programmatically/MyCollectionView/MyCollectionView/SceneDelegate.swift
 //  AppDelegate.swift
 //  MyCollectionView
 //
@@ -30,7 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
 
--- Swift/Programmatically/MyCollectionView/MyCollectionView/ViewController.swift
+++ Swift/Programmatically/MyCollectionView/MyCollectionView/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    var colors: [UIColor] = [
+        .systemCyan, .systemGreen, .systemBlue, .red, .systemOrange, .gray, .systemPurple, .systemYellow, .systemPink
+    ]
+    
+     
     private var collectionView: UICollectionView?
 
     override func viewDidLoad() {
@@ -23,25 +28,58 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collectionView?.delegate = self
         collectionView?.backgroundColor = .white
         view.addSubview(collectionView!)
+        
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture(_ :)))
+        
+        collectionView?.addGestureRecognizer(gesture)
+    }
+    
+    @objc func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer){
+        guard let collectionView = collectionView else {
+            return
+        }
+        switch gesture.state{
+        case .began:
+            guard let targetIndexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else {
+                return
+            }
+            collectionView.beginInteractiveMovementForItem(at: targetIndexPath)
+        case .changed:
+            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: collectionView ))
+        case .ended:
+            collectionView.endInteractiveMovement()
+        default:
+            collectionView.cancelInteractiveMovement()
+        }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         collectionView?.frame = view.bounds
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return colors.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .link
+        cell.backgroundColor = colors[indexPath.row]
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width/3.2,
+                      height: view.frame.size.width/3.2)
+    }
 
-
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = colors.remove(at: sourceIndexPath.row)
+        colors.insert(item, at: destinationIndexPath.row)
+    }
 }
 
